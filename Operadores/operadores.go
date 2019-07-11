abad38dc749f1435519c129a645953a27e42c52a
juliocabrera820 <juliocabrera820@gmail.com>
2019-07-10 22:41:13 -0500

    Operadores

---
+++ Operadores/operadores.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Los operadores son los mismos que en la mayoria de los lenguajes de programacion
+	fmt.Println(3+3)
+	fmt.Println(3/3)
+	fmt.Println(3-3)
+	fmt.Println(3*3)
+	fmt.Println(3%3)
+	//Mostrar la salida de las comparaciones
+	fmt.Println("3==3 es",3==3)
+	fmt.Println("4<3 es",3<3)
+	fmt.Println("4>3 es",3>3)
+	fmt.Println("3!=3 es",3!=3)
+	//Operadores logicos
+	//Y &&
+	//O ||
+	//Otros operadores
+	/*
+		+=
+		-=
+		*=
+		/=
+		%=
+		x++
+		x--
+	*/
+}
